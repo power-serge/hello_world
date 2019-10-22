d41ddc7ec14f7d28e30a1a22d8478a8033dc810c
power-serge <sy120687@hotmail.com>
2019-10-22 14:05:40 -0400

    Changed first and last name to match my first and last name

--- hello.java
+++ hello.java
@@ -5,10 +5,10 @@ public class hello {
     public static void main(String [] args) {
 
 
-        String firstName = "Elizabeth";
-        String lastName = "Jones";
+        String firstName = "Sergio";
+        String lastName = "Yraita, Jr.";
 	
-        System.out.println("Hello Class!");
+        System.out.println("Hello my name is " + firstName +" "+ lastName);
 
 
 
